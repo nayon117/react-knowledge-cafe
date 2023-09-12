de8c9a666c33c1153cddec3bb4947a45bfabcd96
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-12 10:08:20 +0600

    tailwind and react setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+       
+      <h1 className='text-5xl bg-green-500'>React knowledge cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
